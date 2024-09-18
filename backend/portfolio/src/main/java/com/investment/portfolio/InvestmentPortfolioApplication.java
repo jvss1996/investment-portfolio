8085cdb099c83d5db6ea2a120ef39161b702f83a
Jaivardhan <jvss1996@gmail.com>
2024-09-18 13:57:59 +0530

    configured docker for mysql, initial backend project structure, configured db config in backend

---
+++ backend/portfolio/src/main/java/com/investment/portfolio/InvestmentPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.investment.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentPortfolioApplication.class, args);
+	}
+
+}
