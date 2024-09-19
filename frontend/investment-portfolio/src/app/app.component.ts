04ad0765fae1a0ca417d385356f9f796902e8dda
Jaivardhan <jvss1996@gmail.com>
2024-09-19 19:54:19 +0530

    initial frontend from angular version 16

--- frontend/investment-portfolio/src/app/app.component.ts
+++ frontend/investment-portfolio/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'investment-portfolio';
